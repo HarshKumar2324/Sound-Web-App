7c315feeb26234a5ae81c588fa42ee4e3993c6b1
HarshKumar2324 <93465295+HarshKumar2324@users.noreply.github.com>
2022-06-26 15:37:23 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -6,9 +6,21 @@ function preload(){
 
 function setup(){
  canvas=createCanvas(600, 500);
- canvas.position(200, 200);
+ canvas.position(300, 200);
  video=createCapture(VIDEO);
  video.hide();
+ posenet=ml5.poseNet(video, modelloaded);
+ posenet.on('pose', gotposes);
+}
+
+function modelloaded(){
+    console.log("Posenet is initalised");
+}
+
+function gotposes(results){
+if(results.length>0){
+    console.log(results);
+}
 }
 
 function draw(){
@@ -17,4 +29,6 @@ function draw(){
 
 function play(){
     song.play();
+    song.setVolume(1);
+    song.rate(1.5);
 }
